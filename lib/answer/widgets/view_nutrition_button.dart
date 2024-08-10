5e75ad30ed3a948bcff476e87c318703c87b8898
Jaewoo Kim <jaeooo.kim@gmail.com>
2024-08-10 21:45:46 +0900

    Add navigator from answer to nutrition

--- lib/answer/widgets/view_nutrition_button.dart
+++ lib/answer/widgets/view_nutrition_button.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:omma_watson_flutter/nutrition_fact/nutrition_fact_screen.dart';
 
 class ViewNutritionButton extends StatelessWidget {
-  const ViewNutritionButton({super.key});
+  const ViewNutritionButton({
+    super.key,
+    required this.foodName,
+  });
+
+  final String foodName;
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +17,22 @@ class ViewNutritionButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         color: const Color(0xFFE4ECF1),
       ),
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text('영양소 함량 보기'),
-          Icon(
-            Icons.arrow_forward_ios_rounded,
-            size: 16,
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (ctx) => NutritionFactScreen(foodName: foodName),
           ),
-        ],
+        ),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('영양소 함량 보기'),
+            Icon(
+              Icons.arrow_forward_ios_rounded,
+              size: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
