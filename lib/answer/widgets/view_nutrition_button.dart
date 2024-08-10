3468d5e30921a495e771bfa825e40fb11b1b48d8
Seunghyun Min <i@seunghyun.in>
2024-08-11 01:48:52 +0900

    Update ViewNutritionButton

--- lib/answer/widgets/view_nutrition_button.dart
+++ lib/answer/widgets/view_nutrition_button.dart
@@ -11,32 +11,38 @@ class ViewNutritionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: const Color(0xFFF3F6F8),
-      ),
-      child: GestureDetector(
+    return Material(
+      borderRadius: BorderRadius.circular(8),
+      child: InkWell(
         onTap: () => Navigator.of(context).push(
           MaterialPageRoute(
             builder: (ctx) => NutritionFactScreen(foodName: foodName),
           ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'View Nutritional Information',
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: const Color(0xFF474660),
-                  ),
-            ),
-            const Icon(
-              Icons.arrow_forward_ios_rounded,
-              size: 16,
-            ),
-          ],
+        borderRadius: BorderRadius.circular(8),
+        child: Ink(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey.shade100,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'View Nutritional Information',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.grey.shade900),
+              ),
+              Icon(
+                Icons.arrow_forward_ios_rounded,
+                size: 16,
+                color: Colors.grey.shade700,
+              ),
+            ],
+          ),
         ),
       ),
     );
